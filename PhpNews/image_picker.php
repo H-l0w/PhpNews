e08ce3a73c769cad50a0fdf84763ec80feccd53a
vobo470@gmail.com <vobo470@gmail.com>
2022-05-16 21:46:31 +0200

    image picker bug fix

--- PhpNews/image_picker.php
+++ PhpNews/image_picker.php
@@ -16,12 +16,12 @@ $images = $imageRepo->getImages();
     <?php foreach ($images as $image):?>
         <?php if((isset($category) && $category['path'] === $image['path']) || (isset($user) && $user['path'] === $image['path']) || (isset($article) && $article['path'] === $image['path'])):?>
             <label>
-                <input type="radio" name="image" value="<?=$image['id']?>>" checked required>
+                <input type="radio" name="image" value="<?=$image['id']?>" checked required>
                 <img src="<?=$image['path']?>" alt="<?=$image['name']?>">
             </label>
         <?php else:?>
             <label>
-                <input type="radio" name="image" value="<?=$image['id']?>>" required>
+                <input type="radio" name="image" value="<?=$image['id']?>" required>
                 <img src="<?=$image['path']?>" alt="<?=$image['name']?>">
             </label>
         <?php endif;?>
